19bf39e534701743001bf5aa99d90e523665c7c6
Jan Willem Huising <jw.huising@gmail.com>
2019-10-13 23:44:09 +0200

    Update Lib_2D_JS_Biirra.js
    Replaced sprite to other file.
    Removed collision since its going to change.
    Added angle to spin entity's
    Added mover and oscilator for tests.

--- Lib_2D_JS_Biirra.js
+++ Lib_2D_JS_Biirra.js
@@ -93,167 +93,6 @@ class Colider2d{
         return this.entity;
     }
 }
-
-/*
-A sprite is used to display a img or animation on the canvas. 
-
-with inspiration from:
-http://www.williammalone.com/articles/create-html5-canvas-javascript-sprite-animation/
-
-TODO:   Add way of scaling the image.
-*/
-class Sprite{
-    _frameIndex = 0;                                            // The current frame to be displayed
-    _frameRow =  0;                                             // The current row to be displayed
-    _tickCount = 0;                                             // The number updates since the current frame was first displayed
-    
-    constructor(options){
-        this.context = options.context;                         // The canvas this sprite is going to be drawn upon.
-
-        this._location = options.location || Vector2d.zero;     // The location on the canvas where the sprite will be drawn. 
-        this._angle = options.angle || 0;
-
-        this.width = options.width || 0;                        // Height of sprite on the spritesheet in pixels.
-        this.height = options.height || 0;                      // Width of sprite on the spritesheet in pixels.
-
-        this._img = options.img;                                // The spritesheet that belongs to this sprite.
-        this._visible = this._img !== undefined                 // Sprite can be drawn. TODO: include somekind of warning or handle diffrently;
-        
-                         
-        this.numberOfFrames = options.numberOfFrames    || 1;   // The number of frames your spritesheet contains.
-        this.numberOfRows   = options.numberOfRows      || 1;   // If your sprite contains more rows select the correct row to animate.
-        this.ticksPerFrame  = options.ticksPerFrame    || 1;   // The number updates until the next frame should be displayed. Speed is calculeted by window.requestAnimationFrame / this.ticksPerFrame (i.e.: 60fps/4 = 16fps)
-        this.loop = options.loop || false;                      // The animation will loop or not.
-        this.reverse = options.reverse || false;                // Determines if the animation will play in reverse.
-    }
-    update() {
-        this._tickCount += 1;
-        if (this._tickCount > this.ticksPerFrame) {
-            this._tickCount = 0;
-            if(!this.reverse)
-                this.nextFrame();
-            else
-                this.previousFrame();
-        }
-    }
-    previousFrame(){
-        // If the current frame index is in range
-        if (this._frameIndex > 0) {	
-            // Go to the next frame
-            this._frameIndex -= 1;
-        }
-        else if(this._frameRow > 0){
-            this._frameIndex = this.numberOfFrames-1;
-            this._frameRow -= 1;
-        }
-        else if (this.loop){
-            this._frameIndex = this.numberOfFrames-1;
-            this._frameRow = this.numberOfRows-1;
-        }
-    }
-    nextFrame(){
-        // If the current frame index is in range
-        if (this._frameIndex < this.numberOfFrames - 1) {	
-            // Go to the next frame
-            this._frameIndex += 1;
-        }	
-        else if(this._frameRow < this.numberOfRows -1){
-            this._frameIndex = 0;
-            this._frameRow += 1;
-        }
-        else if (this.loop){
-            this._frameIndex = 0;
-            this._frameRow = 0;
-        }
-    }
-    // draw's itself to the canvas.
-    render(){
-        if(!this.visible)
-            return;      
-        
-        this.draw();
-    }
-    draw(){
-        if(this.context === undefined){
-            console.error("Context is not found.");
-            console.error(this);
-            this._visible = false;
-            return;
-        }
-
-        let offsetCenterX = this.width / 2;
-        let offsetCenterY = this.height / 2;
-        this.context.translate(this.location.x+offsetCenterX, this.location.y+offsetCenterY);
-        this.context.rotate(this.angle);
-        this.context.drawImage(
-            this._img,                          //img	Source image        object	Sprite sheet
-            this._frameIndex*this.width,        //sx	Source x	        Frame index times frame width
-            this._frameRow*this.height,         //sy	Source y	        Frame row times frame height
-            this.width,                         //sw	Source width	    Frame width
-            this.height,                        //sh	Source height	    Frame height
-            -offsetCenterX,                     //dx	Destination x	    0 - this.width / 2
-            -offsetCenterY,                     //dy	Destination y	    0 - this.height / 2
-            this.width,                         //dw	Destination width	Frame width
-            this.height                         //dh	Destination height	Frame height
-            );
-        this.context.rotate(-this.angle);
-        this.context.translate(-(this.location.x+offsetCenterX), -(this.location.y+offsetCenterY));
-    }
-    set angle(angle){
-        this._angle = angle;
-    }
-    get angle(){
-        return this._angle;
-    }
-    /**
-     * Set the location where the object will be drawn on the canvas.
-     * @param {Vector2d} vector2d
-     */
-    set location(vector2d){
-        this._location = vector2d;
-    }
-    /**
-     * Get the location of the object. Returns a Vector2d
-     * @returns {Vector2d} Returns the current location of this object.
-     */
-    get location(){
-        return this._location;
-    }
-    /**
-     * Set a new image sprite sheet.
-     * @param {string} src The source string of where the image is location in your folder.
-     */
-    set img(src){
-        this._img = new Image(); 
-        this._img.src = src;
-    }
-    /**
-     * Get the current image spritesheet. Returns a html img element.
-     * @returns {Image} Returns the image element of this object.
-     */
-    get img(){
-        return this._img;
-    }
-    /**
-     * Set the visibility of the object.
-     * @param {boolean} visible
-     */
-    set visible(visible){
-        this._visible = visible;
-    }
-    /**
-     * Get the visibility of the object.
-     * @returns {boolean}
-     */
-    get visible(){
-        return this._visible;
-    }
-    // Toggle the visibility of the object. If false the object will update but will not be drawn to canvas.
-    toggleVisible(){
-        this._visible = !this._visible;
-    }
-}
-
 /*
 TODO: Add way to rotate object.
 */
@@ -268,31 +107,6 @@ class Entity extends Sprite{
         this._aAcceleration = 0;    // Angular acceleration.
 
         this.mass = options.mass || 1;
-        
-    }
-    /**
-     * Detect if self is touching the targets.
-     * @param  {Sprite} target     The sprite object it comes into contact with.
-     * @param  {Vector2d} [offset=Vector2d.zero]
-     * @return {boolean}
-     * 
-     * TODO:    Test if things work correctly if self.velocity is zero and target is hitting self.
-     *          Change this so it will use a collision box.
-     */
-    onCollisionEnter(target, offset=Vector2d.zero){
-        return this.location.x < target.location.x + target.width + offset.x   // check collision with the right side of target
-            && this.location.x + this.width + offset.x > target.location.x     // check collision with the left side of target
-            && this.location.y < target.location.y + target.height + offset.y  // check collision with the bottom side of target
-            && this.location.y + this.height + offset.y > target.location.y;   // check collision with the top side of target
-    }
-    /**
-     * Check if self is not touching the target. Note that target needs to have a bigger width and height than self to work propperly.
-     * @param   {Sprite} target 
-     * @param   {Vector2d} [offset=Vector2d.zero] 
-     * @returns {boolean}
-     */
-    onCollisionLeave(target, offset=Vector2d.zero){
-        return !this.onCollisionEnter(target, offset);
     }
     update(){
         super.update();
@@ -300,7 +114,9 @@ class Entity extends Sprite{
         this.location.add(this.velocity);
         this.acceleration.mult(0);
 
-        this.angle = Math.atan2(this.velocity.y,this.velocity.x);
+        this.aVelocity += this.aAcceleration;
+        this.angle += this.aVelocity; 
+        this.aAcceleration = 0;
     }
     /**
      * Add a force to the object that influenses the location of this object.
@@ -308,7 +124,22 @@ class Entity extends Sprite{
      */
     applyForce(force){
         let f = Vector2d.div(force, this.mass);
-        this.acceleration.add(force);
+        this.acceleration.add(f);
+    }
+    applySpin(number){
+        this.aAcceleration += number;
+    }
+    get aAcceleration(){
+        return this._aAcceleration;
+    }
+    set aAcceleration(number){
+        this._aAcceleration = number;
+    }
+    get aVelocity(){
+        return this._aVelocity;
+    }
+    set aVelocity(number){
+        this._aVelocity = number;
     }
     set acceleration(vector2d){
         this._acceleration = vector2d;
@@ -324,5 +155,40 @@ class Entity extends Sprite{
     }
     kill(){
         this.alive = false;
+    }
+}
+
+class Mover extends Entity{
+    constructor(options){
+        super(options);
+    }
+    update(){
+        super.update();
+        this.angle = Math.atan2(this.velocity.y,this.velocity.x);
+    }
+}
+class Oscillator extends Entity{
+    constructor(options){
+        super(options);
+        this.r = 75;
+        this.theta = 0;
+        this.amplitude = 100;
+        this.period = 120;
+    }
+    draw(){
+        let offsetCenterX = this.width / 2;
+        let offsetCenterY = this.height / 2;
+
+        let x = this.r * Math.cos(this.theta);
+        //let x = this.amplitude * Math.cos((Math.PI*2) * this._tickCount / this.period);
+        let y = this.r * Math.sin(this.theta);
+        //let y = this.amplitude * Math.cos((Math.PI*2) * this._tickCount / this.period);
+
+        this.context.translate(this.location.x+offsetCenterX, this.location.y+offsetCenterY);
+        this.context.beginPath();
+        this.context.ellipse(x+this.width/2, y+this.height/2, this.width, this.height, this.angle, 0, 2 * Math.PI);
+        this.context.stroke();
+        this.context.translate(-(this.location.x+offsetCenterX), -(this.location.y+offsetCenterY));
+        this.theta += 0.01;
     }
 }
