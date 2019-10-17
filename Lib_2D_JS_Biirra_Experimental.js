b4f4075ed5f512f6733669fe77c542ab493d2d53
Jan Willem Huising <jw.huising@gmail.com>
2019-10-17 17:12:01 +0200

    Create Lib_2D_JS_Biirra_Experimental.js
    Created for classes that have a unknown future

---
+++ Lib_2D_JS_Biirra_Experimental.js
@@ -0,0 +1,63 @@
+/*
+Colider
+*/
+class Colider2d{
+    constructor(entity){
+        this._entity = entity;          // Contains the owner.
+        this._collisionTop = false;
+        this._collisionBottom = false;
+        this._collisionRight = false;
+        this._collisionLeft = false;
+    }
+    checkEdges(layer) { // checks if it has reached a side of the colider.
+        // check for collision on x axis
+        if (this._entity.location.x > layer.width) {
+            this.collisionRight = true;
+        } else if (this._entity.location.x < 0) {
+            this.collisionLeft = true;
+        } else {
+            this.collisionRight = false;
+            this.collisionLeft = false;
+        }
+
+        // check for collisions on y axis
+        if (this._entity.location.y > layer.height) {
+            this.collisionBottom = true;
+        }
+        else if (this._entity.location.y < 0) {
+            this.collisionTop = true;
+        }
+        else {
+            this.collisionTop = false;
+            this.collisionBottom = false;
+        }
+        
+    }
+    get collisionTop(){
+        return this._collisionTop;
+    }
+    set collisionTop(value){
+        this._collisionTop = value;
+    }
+    get collisionBottom(){
+        return this._collisionBottom;
+    }
+    set collisionBottom(value){
+        this._collisionBottom = value;
+    }
+    get collisionRight(){
+        return this._collisionRight;
+    }
+    set collisionRight(value){
+        this._collisionRight = value;
+    }
+    get collisionLeft(){
+        return this._collisionLeft;
+    }
+    set collisionLeft(value){
+        this._collisionLeft = value;
+    }
+    get entity(){
+        return this.entity;
+    }
+}
