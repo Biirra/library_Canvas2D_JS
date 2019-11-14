e5c7713332b17af43bd12d80017ef0175937707c
Jan Willem Huising <jw.huising@gmail.com>
2019-11-14 23:48:22 +0100

    Update Lib_2D_JS_Biirra_Experimental.js
    Bugg fixes on particle.

--- Lib_2D_JS_Biirra_Experimental.js
+++ Lib_2D_JS_Biirra_Experimental.js
@@ -62,312 +62,169 @@ class Colision_Body{
     }
 }
 
-class Ball extends Entity{
+/**
+ * A default particle to be used in a particle system.
+ */
+class Particle extends Animated_Sprite{
+    _velocity = Vector2d.zero;
+    _acceleration = Vector2d.zero;
+    _ticksAlive = 1;   // amount of time the particle stay's alive. Measured in fps updates. TODO: Should be able to set this.
     constructor(options){
         super(options);
-        this.impactScale = new Vector2d(0.75,0.75);
-        this.blounceSpeed = 0.01;
+        this.acceleration = options.acceleration || Vector2d.zero;
+
+        // TODO: Fadeout animation can probably belong to a Animator class
+        this.fadeOut = options.fadeOut || false;
+        this.fadeOutSpeed = options.fadeOutSpeed || 0.01;
+        this.ticksAlive = options.ticksAlive || 100;
+
+        this.type = options.type;  // Some default options that can be chosen.
+
+        this.init();
     }
-    bounce(){
-        let maxSizeX = this.original.scale.x + this.impactScale.x;
-        let maxSizeY = this.original.scale.y + this.impactScale.y;
-        let minSizeX = this.original.scale.x - this.impactScale.x;
-        let minSizeY = this.original.scale.y - this.impactScale.y;
-
-        // go bigger if
-        //  its not above max
-        //  its not going smaller.
-        let max = this.scale.x > maxSizeX && this.scale.y > maxSizeY;
-        let min = this.scale.x < minSizeX && this.scale.y < minSizeY;
-        if(max){
-            this._animatingEnlarge = false;
-            this.scale.x = maxSizeX;
-            this.scale.y = maxSizeY;
-        }
-        else if(!max && !this._animatingDiminish){
-            this.scale.x += this.blounceSpeed;
-            this.scale.y += this.blounceSpeed;
-            if(!this._animatingEnlarge){
-                this._animatingEnlarge = true;
-                this._animatingDiminish = false;
-            }
-        }
-
-        // go smaller if
-        //  its not below min
-        //  its not going bigger.
-        if (min){
-            this._animatingDiminish = false;
-            this.scale.x = minSizeX;
-            this.scale.y = minSizeY;
-            //this._jumping = false;
-        }
-        else if(!min && !this._animatingEnlarge){
-            this.scale.x -= this.blounceSpeed;
-            this.scale.y -= this.blounceSpeed;
-            if(!this._animatingDiminish){
-                this._animatingDiminish = true;
-                this._animatingEnlarge = false;
-            }
+    init(){
+        switch(this.type){
+            case "example":
+                this.velocity = new Vector2d(Math.randomFloatBetween(-4,4), Math.randomFloatBetween(-2,0)); 
+                this.frameIndex = new Vector2d(Math.randomIntBetween(0, this.numberOfFrames - 1), Math.randomIntBetween(0, this.numberOfRows - 1));
+                break;
+            default:
+                break;
         }
     }
-}
+    update() {
+        super.update();
+        this._ticksAlive -= 1;
+        if(this.fadeOut && this.alpha > 0)
+            this.alpha = this.fadeOutSpeed*this._ticksAlive;
+        this._velocity.add(this._acceleration);
+        this.body.location.add(this._velocity);
+    }
+    set velocity(value){
+        this._velocity = value;
+    }
+    get velocity(){
+        return this._velocity;
+    }
+    set acceleration(value){
+        this._acceleration = value;
+    }
+    get acceleration(){
+        return this._acceleration;
+    }
+    set ticksAlive(value){
+        this._ticksAlive = value;
+    }
+    get ticksAlive(){
+        return this._ticksAlive;
+    }
+    get alive() {
+        if (this._ticksAlive <= 0.0) 
+            return false;
+        return true;
+    }
+    /**
+     * Generates a copy of itself and returns it.
+     * @returns {Particle} copy of self
+     */
+    get copy(){
+        return new Particle({
+            // Sprite specific
+            context:        this.context,                       // The context.
+            texture:        this.texture,                       // The image. May be a Image or a string of the source.
+            sourceFrame:    this.sFrame.copy,                   // The rectangle data of the selection it will take from the texture image.
+            bodyOptions:    this.body.copy,                     // The rectangle that defines the body of the sprite. Contains data on where to write the sprite on the canvas.
+            rotation:       this.rotation,                      // The rotation.
 
-
-/*
-Seem to be making use alot of this context so creating a class for it.
-Class consists of a location, height and width.
-*/
-class Rect{
-    _location = Vector2d.zero;
-    _width = 0;
-    _height = 0;
-    constructor(width = 0, height = 0, location = Vector2d.zero){
-        this.location = location;
-        this.height = height;
-        this.width = width;
-    }
-    set location(vector2d){
-        this._location = vector2d;
-    }
-    get location(){
-        return this._location;
-    }
-    set height(number){
-        this._height = number;
-    }
-    get height(){
-        return this._height;
-    }
-    set width(number){
-        this._width = number;
-    }
-    get width(){
-        return this._width;
-    }
-    static get zero(){
-        return new Rect();
+            // Animated Sprite specific
+            numberOfFrames: this.numberOfFrames,                // The amount of frames horizontaly on the spritesheet. Left to right.
+	        numberOfRows:   this.numberOfRows,                  // The amount of frames vertically on the spritesheet. Top to bottom.
+	        loop:           this.loop,                          // The animation will start over if its finished.
+            reverse:        this.reverse,                       // Play the animation in reverse
+            ticksPerFrame:  this.ticksPerFrame,                 // The speed it will loop trough it's frames. Will skip frames if below 1 to mimic speedup.
+            
+            // Particle specific
+            fadeOut:        this.fadeOut,                       // Particle will fade out if its time.
+            fadeOutSpeed:   this.fadeOutSpeed,                  // The speed at which the particle fades out. 
+            acceleration:   this.acceleration.copy,             // Mainly contains outside forces that this is affected by.
+            ticksAlive:     this.ticksAlive,                    // The amount of ticks it will stay alive for.
+            type:           this.type
+        });
     }
 }
 
 /**
- * Sprite texture. 
- * Takes a Spritesheet or a Large image and take controll over which section to show on canvas.
+ * A particle system that will create and show set particle.
  */
-class Sprite {
-    // TODO: Use Rect instead
-    _sFrame = Rect.zero;                    // Data containing the location, height and width of the frame taken from the sprites texture.
-    _body = Rect.zero;                      // Data containing the location, height and width of self. Texture scales with this.
+class ParticleSystem {
+    particles = [];    // An array for all the particles
+    _tickCount = 0;
+    constructor(options) {                     
+        this.location = options.location || Vector2d.zero;      // An origin point for where particles are birthed
+        this.maxNoP = options.maxNumberOfParticles || 1;        // The maximum amount of pixels this system can have at the moment of existing.
 
-    _offset = Vector2d.zero;                // TODO: say what it does
+        this.originParticle = options.originParticle;           // The original particle. All spawned particles will be copy's
+        this.spawnSpeed = options.spawnSpeed || 1;              // Amount of ticks that need to pass before it can spawn a new particle.    
+        this.batchSize = options.batchSize || 1;                // The amount of particles that spawn per update.
 
-    _visible = true;                        // Object can be drawn on the canvas.
-    constructor(options){
-        this.context    = options.context;
-        this._texture   = options.texture;
-        this.alpha = options.alpha || 1;
+        this.paused = options.paused || false;                  // If true it will not create any new particles.
 
-        this.sLocation  = options.sLocation     || Vector2d.zero;
-        this.sWidth     = options.sWidth        || 0;
-        this.sHeight    = options.sHeight       || 0;
-
-        this.location   = options.location      || Vector2d.zero;
-        this.anchor     = options.anchor        || Vector2d.zero;
-        this.scale      = options.scale         || Vector2d.one;
-        this.offset     = options.offset        || Vector2d.zero;
-        this.width      = options.width         || this.sWidth;
-        this.height     = options.height        || this.sHeight;
-        
-        this.rotation   = options.rotation      || 0;
+        this.init();
     }
-    render(){ 
-        this.location.sub(this.offset);
-        if(this.visible)
-            this.draw();
+    init(){
+        //this.addOriginParticleByBatch(this.batchSize);
     }
-    draw(){ 
-        this.context.globalAlpha = this.alpha;
-        this.context.translate(this.location.x, this.location.y);
-        this.context.rotate(this.rotation);
-        this.context.drawImage(
-            this.texture,               //img	Source image 
-            this._sFrame.location.x,    //sx	Source x	        
-            this._sFrame.location.y,    //sy	Source y	        
-            this._sFrame.width,         //sw	Source width	   
-            this._sFrame.height,        //sh	Source height	   
-            0,                          //dx	Destination x	 
-            0,                          //dy	Destination y	   
-            this.width,                 //dw	Destination width	
-            this.height                 //dh	Destination height	
-            );
-        this.context.rotate(-this.rotation);
-        this.context.translate(-this.location.x, -this.location.y);
-        this.context.globalAlpha = 1;
-    }
-    /**
-     * Readonly the offset from the original 0,0 position.
-     */
-    get offset(){
-        return this._offset;
-    }
-    set offset(vector2d){ // TODO: It works but be sure of what this is supposed to do and check if it works as expected. 
-
-        // Calculate offset
-        // increase width by scale and apply the anchor
-        let width = this.width * this.scale.x;
-        let widthOffset = width  * this.anchor.x;
-        // increase height by scale and apply the anchor
-        let height = this.height * this.scale.y;
-        let heightOffset = height * this.anchor.y;
-        
-        // set offset
-        this._offset = new Vector2d(widthOffset, heightOffset);
-
-        // apply custom offset.
-        this._offset.add(vector2d);
-    }
-    /**
-     * texture
-     * @param {Image||string} image set the texture with either an Image object or a (String)src of the image. Img object reccomended
-     */
-    set texture(image){
-        if(typeof image === string){
-            let result = new Image();
-            result.src = image;
-            this._texture = result;
-            if(this.developerMode){
-                Console.warn("New Image created for this.texture. If same source is used repeatedly, consider defining it first as a new Image(src) and use that instead.");
-                Console.warn(this);
+    update() {
+        this._tickCount += 1;
+        // Cycle through the ArrayList backwards b/c we are deleting
+        for (let i = this.particles.length-1; i >= 0; i--) {
+            let particle = this.particles[i];
+            particle.update();
+            if (!particle.alive) {
+                this.particles.splice(i,1);
             }
-            return;
         }
-        if(typeof image === Image){
-            this._texture = image;
-            return;
+        
+        // if allowed and its time to create a new particle.
+        if(this._tickCount >= this.spawnSpeed && !this.paused){
+            this._tickCount = 0;
+            // if there is room for new particles add new particles
+            this.addOriginParticleByBatch(this.batchSize);
         }
-            
     }
-    get texture(){
-        return this._texture;
+    renderChildren(){
+        // cylcle backwards so the new particles are displayed behind older particles.
+        for (let i = this.particles.length-1; i >= 0; i--) {
+            this.particles[i].render();
+        }
     }
-    /**
-     * Source location.
-     * @param {Vector2d} vector2d
-     */
-    set sLocation(vector2d){
-        this._sFrame.location.x = vector2d.x;
-        this._sFrame.location.y = vector2d.y;
+    addOriginParticleByBatch(batchSize){
+        if(this.maxNoP+batchSize > this.particles.length){
+            for(let i = 0; i < batchSize; i++){
+                this.addOriginParticle();
+            }
+        }
     }
-    get sLocation(){
-        return this._sFrame.location;
+    addOriginParticle(){
+        let p = this.originParticle.copy;
+        p.body.location = this.location.copy;
+        this.particles.push(p);
     }
-    /**
-     * Source height.
-     * @param {number} number
-     */
-    set sHeight(number){
-        this._sFrame.height = number;
+    addParticleByBatch(particle, batchSize){
+        if(this.maxNoP+batchSize > this.particles.length){
+            for(let i = 0; i < batchSize; i++){
+                this.addParticle(particle);
+            }
+        }
     }
-    get sHeight(){
-        return this._sFrame.height;
+    addParticle(particle){
+        let p = particle.copy;
+        p.body.location = this.location.copy;
+        this.particles.push(p);
     }
-    /**
-     * Source width.
-     * @param {number} number
-     */
-    set sWidth(number){
-        this._sFrame.width = number;
-    }
-    get sWidth(){
-        return this._sFrame.width;
-    }
-
-    /**
-     * Destination location.
-     * @param {Vector2d} vector2d
-     */
-    set location(vector2d){
-        this._body.location = vector2d.copy;
-    }
-    get location(){
-        return this._body.location;
-    }
-    /**
-     * Destination height.
-     * @param {number} number
-     */
-    set height(number){
-        this._body.height = number*this.scale.y;
-    }
-    get height(){
-        return this._body.height;
-    }
-    /**
-     * Destination width.
-     * @param {number} number
-     */
-    set width(number){
-        this._body.width = number*this.scale.x;
-    }
-    get width(){
-        return this._body.width;
-    }
-    set anchor(vector2d){
-        this._anchor = vector2d;
-    }
-    get anchor(){
-        return this._anchor;
-    }
-    /**
-     * Set the rotation of the texture.
-     * @param {number} radian
-     */
-    set rotation(radian){
-        this._rotation = radian;
-    }
-    /**
-     * Get the rotation of the texture.
-     * @returns {number} 
-     */
-    get rotation(){
-        return this._rotation;
-    }
-    set scale(vector2d){
-        this._scale = vector2d;
-    }
-    get scale(){
-        return this._scale;
-    }
-    set visible(boolean){
-        this._vivible = boolean;
-    }
-    get visible(){
-        if(!this.context){
-            // TODO: Fix this so it will not print forever than reactivate warnings if in developer mode.
-            //console.warn("Context not found. Visibility is set to false by force.");
-            //console.warn(this);
+    get alive(){
+        if (this.particles.length === 0) 
             return false;
-        }
-        return this._visible;
+        return true;
     }
-    /**
-     * Generates a copy of itself and returns it.
-     * @returns {Sprite} copy of self
-     */
-    get copy(){
-        let result = new Sprite({
-            context:        this.context,                       // The context.
-            texture:        this.texture,                       // The image. May be a Image or a string of the source.
-            sLocation:      this.sLocation.copy,                // Top right location of the selected area on the image.
-            sWidth:         this.sWidth,                        // The height of the selected area on the image.
-            sHeight:        this.sHeight,                       // The Widht of the selected area on the image.
-            location:       this.location.copy,                 // The location where to draw the selected area on the canvas.
-            anchor:         this.anchor.copy,                   // The center of the selected area. Vector.one = bottom right corner.
-            width:          this.width,                         // The width that scales the selected area.
-            height:         this.height,                        // The height that scales the selected area.
-            scale:          this.scale.copy,                    // The scale of the selected area.
-            rotation:       this.rotation,                      // The rotation.
-        });
-        return result;
-    }
-}
+} 
