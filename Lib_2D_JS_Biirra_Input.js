da88313530490e1ca627408e686e2da5b0e57be0
Jan Willem Huising <janwillem@iraiser.nl>
2019-11-19 14:51:21 +0100

    Update Lib_2D_JS_Biirra_Input.js
    Added key handlers.
    Not sure yet tho how this will fit in the complete library.

--- Lib_2D_JS_Biirra_Input.js
+++ Lib_2D_JS_Biirra_Input.js
@@ -2,7 +2,13 @@ class InputListener{
     constructor(canvas){
         this.canvas = canvas;
         this._mousePosition = Vector2d.zero;
-
+        this._mouseState = {
+            clicked:false,
+            pressed:false,
+            released:false,
+            drag:false
+        }
+        this._keyState = {};
         this.init();
     }
     init(){
@@ -11,11 +17,23 @@ class InputListener{
     }
     setupHandlers(){
         this.updateMousePositionHadler  = this.updateMousePosition.bind(this);
+        this.updateKeyDownHandler = this.updateKeyDown.bind(this);
+        this.updateKeyUpHandler = this.updateKeyUp.bind(this);
 		return this;
     }
     enable(){
         this.canvas.addEventListener('mousemove', this.updateMousePositionHadler);
+        document.addEventListener('keypress', this.updateKeyDownHandler);
+        document.addEventListener('keyup', this.updateKeyUpHandler);
 		return this;
+    }
+    updateKeyDown(event){
+        this._keyState[event.code] = event;
+        this._keyState[event.code].isDown = true;
+    }
+    updateKeyUp(event){
+        this._keyState[event.code] = event;
+        this._keyState[event.code].isDown = false;        
     }
     updateMousePosition(){
         let x = event.clientX || this._mousePosition.x;
